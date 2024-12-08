4f502db8a19e108a5026ddcf59dd511bbe08a3bc
c4ke <artemiy.makar@gmail.com>
2024-12-08 22:50:32 +0300

    Отредактировал имя окна

---
+++ ToDoList/Custom/Native.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Custom;
+
+public class Native
+{
+    [DllImport("ToDoLib.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void MyMessageBox(string message, string title);
+}
